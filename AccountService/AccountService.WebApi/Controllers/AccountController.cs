9443ec40173635a106637ec93de695b666617740
Koryun Grigoryan <koryungrigoryan03@gmail.com>
2025-12-14 15:23:49 +0400

    aa

---
+++ AccountService/AccountService.WebApi/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AccountService.WebApi.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class AccountController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Test()
+    {
+        return Ok();
+    }
+}
